016db4b4b822744ca34365f28464b806d323494c
Joshua Kuehn <josh_qn@Joshuas-MacBook-Air.local>
2014-12-04 11:32:03 -0600

    Initial Commit

---
+++ Taskit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taskit
+//
+//  Created by Joshua Kuehn on 12/4/14.
+//  Copyright (c) 2014 Joshua Kuehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
